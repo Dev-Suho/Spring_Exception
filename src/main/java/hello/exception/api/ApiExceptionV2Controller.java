d463a1fe2e60a72f12a38d98706cd8d77046b60d
Dev-Suho <devsuho99@gmail.com>
2023-04-15 17:03:31 +0900

    ExceptionHandler_0415

--- src/main/java/hello/exception/api/ApiExceptionV2Controller.java
+++ src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -1,19 +1,28 @@
-package hello.exception.interceptor.api;
+package hello.exception.api;
 
 import hello.exception.exception.UserException;
+import hello.exception.exhandler.ErrorResult;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-public class ApiExceptionController {
+public class ApiExceptionV2Controller {
 
-    @GetMapping("/api/members/{id}")
+    @Data
+    @AllArgsConstructor
+    static class MemberDTO {
+        private String memberId;
+        private String name;
+    }
+
+    @GetMapping("/api2/members/{id}")
     public MemberDTO getMember(@PathVariable("id") String id) {
+
         if (id.equals("ex")) {
             throw new RuntimeException("잘못된 사용자");
         }
@@ -28,10 +37,4 @@ public class ApiExceptionController {
         return new MemberDTO(id, "hello " + id);
     }
 
-    @Data
-    @AllArgsConstructor
-    static class MemberDTO {
-        private String memberId;
-        private String name;
-    }
 }
